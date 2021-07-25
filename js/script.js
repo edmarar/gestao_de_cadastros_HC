0e8b5220c66b94b7137764dfcb417d7031a70671
edmarar <edmararocha007@gmail.com>
2021-07-25 20:24:37 -0300

    add files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value;
+  let data = {
+    nome,
+    email,
+  }
+  let convertData = JSON.stringify(data);
+
+  localStorage.setItem('lead', convertData)
+
+  let content = document.getElementById('content')
+
+  let carregando = `<img src='./assets/loader.gif' id='loader'></img>`
+
+  let pronto = `<p>Cadastro realizado com sucesso!</p>`
+
+  content.innerHTML = carregando
+
+
+  setTimeout(() => {
+    content.innerHTML = pronto
+  }, 1000)
+
+})
